48bf4d151b722c59ef7c635e2b03fefcec84415e
Pedro <sebetienne@hotmail.com>
2021-11-25 21:45:42 +0100

    Ajouter nos test de docker etc.

--- apps/api/app/__init__.py
+++ apps/api/app/__init__.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi Pedros, I am the API"
+  return "Hi Pedros, I am the API."
+
+@app.route('/mapage')
+def mapage():
+  return "Ma page test"
